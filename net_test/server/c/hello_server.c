5f72e8552029f26002bf991dc4fb59444aa30535
walter-sarajevo <walter_of_sarajevo@126.com>
2018-07-16 15:22:49 +0800

    add client

--- net_test/server/c/hello_server.c
+++ net_test/server/c/hello_server.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]){
     exit(1);
   }
   
-  serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+  serv_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
   if(serv_sock == -1){
     error_handling("socket() error");
   }
